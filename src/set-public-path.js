2a08518604fe952b3cf28ebbc95492296db1479f
Yetrina Battad <hello@yetti.io>
2020-12-02 12:56:43 +1100

    chore: initial commit

---
+++ src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@yetti/single-spa-demo-page2", 2);
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    This is page 2
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
